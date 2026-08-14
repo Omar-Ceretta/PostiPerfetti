c7fe298cb41df16782e31802cfdd4af6775f157c
Omar Ceretta <ceretta.omar@ictombologalliera.edu.it>
2026-08-14 21:54:30 +0200

    Add files via upload

--- moduli/editor_studenti.py
+++ moduli/editor_studenti.py
@@ -1434,8 +1434,8 @@ class EditorStudentiWidget(QWidget):
         if os.path.exists(percorso_logo):
             pixmap = QPixmap(percorso_logo)
             pixmap = pixmap.scaled(
-                320,
-                150,
+                600,
+                400,
                 Qt.KeepAspectRatio,
                 Qt.SmoothTransformation
             )
